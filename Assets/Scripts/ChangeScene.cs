842b1acc91fc1dfa4ab9e94bb0e3f8a344c8ae6c
JimmyExu <isakamgz@gmail.com>
2018-08-17 08:42:20 -0500

    Escenas Agregadas
    00Splash
    01Login
    02Menu
    03AR
    04ProjectEdit
    05Settings
    06UserInfo
    Codigo viejo@DefaultScene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField]
+    private float delayBeforeScene = 5f;
+    [SerializeField]
+    private string sceneToBeLoaded;
+
+    private float timeElapsed;
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if (timeElapsed > delayBeforeScene)
+        {
+            SceneManager.LoadScene(sceneToBeLoaded);
+        }
+    }
+}
