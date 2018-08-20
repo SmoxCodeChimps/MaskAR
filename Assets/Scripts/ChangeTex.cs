f60386a539cbb24aaa0361dc5e0bb4f04a7b35da
JimmyExu <isakamgz@gmail.com>
2018-08-20 12:06:52 -0500

    SmoxV.0.1.3
    Problemas de escala arreglados.
    Las imagenes ahora persisten entre escenas

--- Assets/Scripts/ChangeTex.cs
+++ Assets/Scripts/ChangeTex.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 public class ChangeTex : MonoBehaviour {
     public string FileName = "C:\\popu.png";
     public GameObject portrait;
+    public Texture2D byebye;
     // Use this for initialization
     void Start () {
 		
@@ -30,14 +31,16 @@ public class ChangeTex : MonoBehaviour {
         return tex;
     }
 
-    public static Texture2D getImage(int maxSize){
+    public static Texture2D setImage(int maxSize){
         Texture2D texture = null;
         NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((path) =>{
             Debug.Log("Image path: " + path);
+            PlayerPrefs.SetString("ARContentImagepath", path);
+            PlayerPrefs.SetInt("FirstTimer", 1);
             if (path != null)
             {
                 // Create Texture from selected image
-                 texture = NativeGallery.LoadImageAtPath(path, maxSize);
+                 texture = NativeGallery.LoadImageAtPath(PlayerPrefs.GetString("ARContentImagepath"), maxSize);
                 if (texture == null){
                     Debug.Log("Couldn't load texture from " + path);
                     return;
@@ -49,12 +52,37 @@ public class ChangeTex : MonoBehaviour {
         Debug.Log("Permission result: " + permission);
         return texture;
     }
+    public static Texture2D setTheImage(int maxSize){
+        Texture2D texture = null;
+        /*NativeGallery.Permission permission = NativeGallery.GetImageFromGallery((path) => {
+            Debug.Log("Image path: " + path);
+            PlayerPrefs.SetString("ARContentImagepath", path);
+            if (path != null)
+            {
+                // Create Texture from selected image
+                texture = NativeGallery.LoadImageAtPath(PlayerPrefs.GetString("ARContentImagepath"), maxSize);
+                if (texture == null)
+                {
+                    Debug.Log("Couldn't load texture from " + path);
+                    return;
+                }
+
+            }
+        }, "Selecciona una imagen PNG", "image/png", maxSize);
+        */
+        if (PlayerPrefs.GetString("ARContentImagepath") != null)
+        {
+            texture = NativeGallery.LoadImageAtPath(PlayerPrefs.GetString("ARContentImagepath"), maxSize);
+        }
+        return texture;
+    }
+
+
     public void Change(){
         GameObject portrait = GameObject.Find("Canvas/Portrato");
        
         //Texture2D tex = LoadPNG(FileName); // FOR TEST IN PC
-        Texture2D tex = getImage(1000); //FOR MObILE DEVICES
-
+        Texture2D tex = setTheImage(1000); //FOR MObILE DEVICES
         /*
         Renderer rend = portrait.GetComponent<Renderer>();
         rend.material.shader = Shader.Find("Transparent/VertexLit with Z");
@@ -78,5 +106,91 @@ public class ChangeTex : MonoBehaviour {
        
         //mat.mainTexture = tex;
 
+    }
+
+    public void ImgLoad(){
+        GameObject portrait = GameObject.Find("Canvas/CustomImage/Loader");
+
+        //Texture2D tex = LoadPNG(FileName); // FOR TEST IN PC
+        Texture2D tex = setImage(1000); //FOR MObILE DEVICES
+        /*
+        Renderer rend = portrait.GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("Transparent/VertexLit with Z");
+        Vector3 angles = rend.transform.localEulerAngles;
+        angles.y = 180;
+        rend.transform.localEulerAngles = angles;
+        rend.material.mainTexture = tex;
+        
+        Debug.Log(portrait.name);
+        */
+        RawImage image = portrait.GetComponent<RawImage>();
+        //image.color = new Color(1F, 1F, 1F, 1); //Changes visibility
+        Material mat = image.material;
+        Debug.Log("portrait color: " + image.color);
+        image.texture = tex;
+        //image.color = new Color(1F, 1F, 1F, 0F);
+        //mat.color = new Color(1F, 1F, 1F, 1F);
+        Debug.Log("Material: " + mat.color);
+        mat.mainTexture = tex;
+        Debug.Log("From button: " + image.texture);
+
+        //mat.mainTexture = tex;
+
+    }
+    public void DoYouEvenNullBrah()
+    {
+        GameObject portrait = GameObject.Find("Canvas/Portrato");
+
+        //Texture2D tex = LoadPNG(FileName); // FOR TEST IN PC
+        Texture2D tex = byebye; //FOR MObILE DEVICES
+        /*
+        Renderer rend = portrait.GetComponent<Renderer>();
+        rend.material.shader = Shader.Find("Transparent/VertexLit with Z");
+        Vector3 angles = rend.transform.localEulerAngles;
+        angles.y = 180;
+        rend.transform.localEulerAngles = angles;
+        rend.material.mainTexture = tex;
+        
+        Debug.Log(portrait.name);
+        */
+        RawImage image = portrait.GetComponent<RawImage>();
+        //image.color = new Color(1F, 1F, 1F, 1); //Changes visibility
+        Material mat = image.material;
+        Debug.Log("portrait color: " + image.color);
+        image.texture = tex;
+        //image.color = new Color(1F, 1F, 1F, 0F);
+        //mat.color = new Color(1F, 1F, 1F, 1F);
+        Debug.Log("Material: " + mat.color);
+        mat.mainTexture = tex;
+        Debug.Log("From button: " + image.texture);
+
+        //mat.mainTexture = tex;
+
+    }
+    public void Preview()
+    {
+        GameObject portrait = GameObject.Find("Canvas/CustomImage");
+        if (PlayerPrefs.GetInt("FirstTimer") != 1)
+        {
+            Texture2D tex = byebye;
+            RawImage image = portrait.GetComponent<RawImage>();
+            Material mat = image.material;
+            Debug.Log("portrait color: " + image.color);
+            image.texture = tex;
+            Debug.Log("Material: " + mat.color);
+            mat.mainTexture = tex;
+            Debug.Log("From button: " + image.texture);
+        }
+        else if (PlayerPrefs.GetInt("FirstTimer") == 1)
+        {
+            Texture2D tex = setImage(1000); //FOR MObILE DEVICES
+            RawImage image = portrait.GetComponent<RawImage>();
+            Material mat = image.material;
+            Debug.Log("portrait color: " + image.color);
+            image.texture = tex;
+            Debug.Log("Material: " + mat.color);
+            mat.mainTexture = tex;
+            Debug.Log("From button: " + image.texture);
+        }
     }
 }
