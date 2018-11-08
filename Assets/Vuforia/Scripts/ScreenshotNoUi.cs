72f3086f2c59aa846c83a1b1c0c0163e74235dac
Jimmy Exu <isakamgz@gmail.com>
2018-11-07 21:42:41 -0600

    Si_nos_dejan
    Buscamos un rincon cerca del cielo
    ;)

---
+++ Assets/Vuforia/Scripts/ScreenshotNoUi.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotNoUi : MonoBehaviour
+{
+
+    public void OnClickScreenCaptureButton()
+    {
+        StartCoroutine(CaptureScreen());
+    }
+    public IEnumerator CaptureScreen()
+    {
+        // Wait till the last possible moment before screen rendering to hide the UI
+        yield return null;
+        GameObject.Find("MyCanvas").GetComponent<Canvas>().enabled = false;
+
+        // Wait for screen rendering to complete
+        yield return new WaitForEndOfFrame();
+
+        // Take screenshot
+        ScreenCapture.CaptureScreenshot("AR4U_Screenshot.png");
+
+        // Show UI after we're done
+        GameObject.Find("MyCanvas").GetComponent<Canvas>().enabled = true;
+    }
+}
